b999c03effef9764bf2e84a7cf6ce5157d73cfd5
Benjamin Wilhelm <benjamin@b-wilhelm.de>
2023-03-14 20:15:05 +0100

    Update layout and theme

--- src/App.tsx
+++ src/App.tsx
@@ -10,25 +10,32 @@ import { JsonForms } from "@jsonforms/react";
 import { schema, uischema, initialData } from "./breadform";
 import RecipeTable from "./RecipeTable";
 import compute from "./compute";
-import { Typography } from "@mui/material";
+import { Paper, Typography } from "@mui/material";
 
 function App() {
   const [data, setData] = useState(initialData);
 
   return (
     <Container maxWidth="sm" sx={{ my: 4 }}>
-      <JsonForms
-        schema={schema}
-        uischema={uischema}
-        data={data}
-        renderers={materialRenderers}
-        cells={materialCells}
-        onChange={({ data, errors }) => setData(data)}
-      />
-      <Typography variant="h3" gutterBottom>
-        Recipe
-      </Typography>
-      <RecipeTable recipe={compute(data)} />
+      <Paper sx={{ p: 4, mb: 4 }} elevation={5}>
+        <Typography variant="h4" gutterBottom>
+          Bread
+        </Typography>
+        <JsonForms
+          schema={schema}
+          uischema={uischema}
+          data={data}
+          renderers={materialRenderers}
+          cells={materialCells}
+          onChange={({ data, errors }) => setData(data)}
+        />
+      </Paper>
+      <Paper sx={{ p: 4, mb: 4 }} elevation={5}>
+        <Typography variant="h4" gutterBottom>
+          Recipe
+        </Typography>
+        <RecipeTable recipe={compute(data)} />
+      </Paper>
     </Container>
   );
 }
